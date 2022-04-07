8daeabf8f37525cef1ccc6632a2cae47ff3b2652
Olamide Atitebi <olamideumarq@gmail.com>
2022-04-07 18:37:45 +0100

    bug fix

--- README.md
+++ README.md
@@ -10,11 +10,8 @@ This is a solution to the [Advice generator app challenge on Frontend Mentor](ht
     -   [Links](#links)
 -   [My process](#my-process)
     -   [Built with](#built-with)
-    -   [What I learned](#what-i-learned)
-    -   [Continued development](#continued-development)
     -   [Useful resources](#useful-resources)
 -   [Author](#author)
--   [Acknowledgments](#acknowledgments)
 
 ## Overview
 
--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 document.getElementById("btn").addEventListener("mousedown", fetchAdvice);
 const quote = document.querySelector(".card__quote");
+const quoteId = document.querySelector(".quote__id");
 
 async function fetchAdvice() {
-    const response = await fetch("https://api.adviceslip.com/advice");
+    const response = await fetch("https://api.adviceslip.com/advice", {
+        method: "GET",
+        cache: "reload",
+    });
     const data = await response.json();
     const { slip } = data;
     quote.innerText = slip.advice;
+    quoteId.innerText = slip.id;
 }
